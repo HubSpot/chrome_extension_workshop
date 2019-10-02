94d6ada91855b080774edb4038ff7bb3b4ce3160
Anna Kutch <akutch@hubspot.com>
2019-10-02 15:15:13 -0400

    make extension download file by default

--- extension/background_three.js
+++ extension/background_three.js
@@ -1,12 +1,14 @@
-// when extension button is clicked
-chrome.browserAction.onClicked.addListener(function() {
-  // take a screenshot of the current tab
-  chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-    // creat the url for the new tab with the screenshot url as a parameter
-    var viewTabUrl = chrome.extension.getURL(
-      "screenshot.html?screenshotUrl=" + screenshotUrl
-    );
-    // create a new tab to show the screenshot
-    chrome.tabs.create({ url: viewTabUrl });
+function downloadScreenshot(screenshotUrl) {
+  // download screenshot file
+  chrome.downloads.download({
+    url: screenshotUrl
   });
-});
+}
+
+function takeScreenshot() {
+  // take a screenshot of the current tab
+  chrome.tabs.captureVisibleTab(downloadScreenshot);
+}
+
+// when extension button is clicked
+chrome.browserAction.onClicked.addListener(takeScreenshot);
